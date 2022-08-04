ee45ed4c01696d3fd2d398c81d7fb09ea4e43d0c
arjun.m <arjunkaarthik92@gmail.com>
2022-08-04 15:42:18 +0530

    first commit

---
+++ dataTypes.py
@@ -0,0 +1,15 @@
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+for i in range(1, 4):
+    print(i)
+    
+for i in range(1, a // b):
+    print(i)
---
+++ strings.py
@@ -0,0 +1,7 @@
+greetings = "Hello"
+name = "John"
+print(greetings + ' ' + name)
+age = 24
+print(age)
+print(type(greetings))
+print(type(age))
